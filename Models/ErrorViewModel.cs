d3a76faf35de26663509524882969efe17379d9c
Joel SOUSA <joel.sousa@altran.com>
2021-11-12 10:01:32 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gestão_Software.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
