5b41409c48d5d5b754ac2dc79e9cb4d39cabbc8c
Joel SOUSA <joel.sousa@altran.com>
2021-11-13 22:57:11 +0000

    Add Project Class and Software Requirement Class

---
+++ Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestão_Software.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        public string Client { get; set; }
+
+        public ICollection<SoftwareRequirement> SoftwareRequirements { get; set; }
+    }
+}
---
+++ Models/SoftwareRequirement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestão_Software.Models
+{
+    public class SoftwareRequirement
+    {
+        public int SoftwareReqId { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string SoftwareReqTitle { get; set; }
+
+        public string SoftwareReqDescription { get; set; }
+
+        public Project Project { get; set; }
+
+    }
+}
