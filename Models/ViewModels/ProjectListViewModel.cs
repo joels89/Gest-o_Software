ff98e1e42740b63a7de7c5c51f4ba860af7845b8
joaos <joaosaraiva.s.js@gmail.com>
2021-11-18 17:25:29 +0000

    Criação do view model para a paginação

---
+++ Models/ViewModels/ProjectListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestão_Software.Models.ViewModels
+{
+    public class ProjectsListViewModel
+    {
+        public IEnumerable<Project> Projects { get; set; }
+        public PagingInfo Pagination { get; set; }
+    }
+}
