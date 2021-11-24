7ae7179b57669bfd93dc95197c39bd82449c4bab
Joel SOUSA <joel.sousa@altran.com>
2021-11-24 22:42:35 +0000

    Adding Projectlist ViewModel to show number of Projects in list.

---
+++ Models/ViewModels/ProjectsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestão_Software.Models.ViewModels
+{
+    public class ProjectsListViewModel
+    {
+        public IEnumerable<Project> Projects { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        
+    }
+}
