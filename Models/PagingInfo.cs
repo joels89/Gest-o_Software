601bedf447533abab31b4efe5445d840f7f7aef0
Joel SOUSA <joel.sousa@altran.com>
2021-11-24 22:34:09 +0000

    Add PagingInfo Class to Project  Gestão_Software

---
+++ Models/PagingInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestão_Software.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int PageSize { get; set; } = 10;
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems/PageSize);
+    }
+}
