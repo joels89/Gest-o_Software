32cba8bbc2b60699f0d5fab48dade42191e9a8e3
joaos <joaosaraiva.s.js@gmail.com>
2022-01-15 16:07:15 +0000

    Adicionada view model para registar clientes

---
+++ ViewModels/RegisterClientViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestão_Software.ViewModels
+{
+    public class RegisterClientViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Nome{ get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Endereco { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Telefone { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar password")]
+        [Compare("Password", ErrorMessage = "A password e confirmação não correspondem.")]
+        public string ConfirmarPassword { get; set; }
+    }
+}
